20c7ad5c5ece499befe3df5ea464cf9ab98d0d43
jenbensen17 <jenbensen17@gmail.com>
2025-02-08 16:10:36 -0700

    Convert login to react component

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './nav.css';
-import './app.css';
+
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import {Login} from './login/login';
 import {Dashboard} from './dashboard/dashboard';
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import './login.css'
 export function Login() {
   return (
-    <main>
-      <div>login displayed here</div>
+    <main className="login-page">
+        <h1>Welcome to DEFCOR-FIT</h1>
+        
+        <form method="get" action="dashboard.html">
+            <div className="login">
+                <span>Email:</span>
+                <input type="text" placeholder="user@email.com" />
+            </div>
+            <div className="login">
+                <span>Password:</span>
+                <input type="password" placeholder="password" />
+            </div>
+            <div>
+                <button type="submit">Login</button>
+                <button type="submit">Register</button>
+            </div>
+        </form>
     </main>
   );
 }
