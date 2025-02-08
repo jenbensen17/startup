51b96c773eb3ca0fba5217a6cb1027c4615e138c
jenbensen17 <jenbensen17@gmail.com>
2025-02-08 15:48:07 -0700

    Create the router

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,51 @@
 import React from 'react';
 import './nav.css';
 import './app.css';
-
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import {Login} from './login/login';
+import {Dashboard} from './dashboard/dashboard';
+import {Workout} from './workout/workout';
 
 export default function App() {
     return (
+    <BrowserRouter>
     <div className='body'>
          <header>
-            <a href="index.html" className="logo">
+            <NavLink to="index.html" className="logo">
                 <img className="logo-img" src="/def-cor.png" />
                 <h1>DEFCOR-FIT</h1>
-            </a>
+            </NavLink>
             <nav>
                 <menu>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="dashboard.html">Dashboard</a></li>
-                    <li><a href="log.html">Log Workout</a></li>
+                    <li><NavLink to=''>Login</NavLink></li>
+                    <li><NavLink to='dashboard'>Dashboard</NavLink></li>
+                    <li><NavLink to="workout">Log Workout</NavLink></li>
                 </menu>
             </nav>
         </header>
 
         <main>App components go here</main>
 
+        {/* // to */}
+
+        <Routes>
+            <Route path='/' element={<Login />} exact />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/workout' element={<Workout />} />
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+
         <footer>
             <span>Made by Ben Jensen</span>
             <br />
-            <a href="https://github.com/jenbensen17/startup">GitHub Repo</a>
+            <NavLink to="https://github.com/jenbensen17/startup">GitHub Repo</NavLink>
         </footer>
 
     </div>
+    </BrowserRouter>
     );
+  }
+
+  function NotFound() {
+    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
   }
