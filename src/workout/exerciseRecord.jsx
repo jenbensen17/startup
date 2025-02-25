df8b7fd5182d539b8b89877b0705d98209896e55
jenbensen17 <jenbensen17@gmail.com>
2025-02-25 13:43:53 -0700

    Add add set and remove set functionality

---
+++ src/workout/exerciseRecord.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+export function ExerciseRecord(props) {
+    const exerciseName = props.exerciseName;
+
+    const [sets, setSets] = React.useState([{weight: '', reps: ''}]);
+    const addSet = () => {
+        setSets([...sets, { weight: '', reps: '' }]);
+      };
+    const removeSet = () => {
+        setSets((prevSets) => {
+            if(prevSets.length>1) {
+                const newSets = [...prevSets];
+                newSets.pop();
+                return newSets;
+            } else {
+                return prevSets;
+            }
+        })
+    }
+
+    return (
+        <>
+        <div className="record">
+        <div className="exercise">
+                <img src="/dumbbell.png" width="25"/>
+                <h3> 
+                    Bench Press
+                </h3>
+            </div>
+        <table>
+
+            <thead>
+            <tr>
+                <th>Set</th>
+                <th>Weight</th>
+                <th>Reps</th>
+            </tr>
+            </thead>
+            <tbody>
+          {sets.map((set, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>
+                <input
+                  type="text"
+                  placeholder="--"
+                  value={set.weight}
+                  onChange={(e) => {
+                    const newSets = [...sets];
+                    newSets[index].weight = e.target.value;
+                    setSets(newSets);
+                  }}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  placeholder="--"
+                  value={set.reps}
+                  onChange={(e) => {
+                    const newSets = [...sets];
+                    newSets[index].reps = e.target.value;
+                    setSets(newSets);
+                  }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        </table>
+        <button className="set-btn" type="button" onClick={addSet}>+ Add Set</button>
+        {sets.length > 1 && <button className="set-btn" type="button" onClick={removeSet}>- Remove Set</button>}
+        </div>
+        </>
+    )
+}
--- src/workout/workout.jsx
+++ src/workout/workout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './workout.css'
+import { ExerciseRecord } from './exerciseRecord';
 
 export function Workout() {
   return (
@@ -7,58 +8,11 @@ export function Workout() {
     <form method="get" action='index.html'>
         <h2>LOG YOUR WORKOUT HERE</h2>
     <button type="button">Save and Record</button>
-    <div>
-    <div className="exercise">
-                <img src="/dumbbell.png" width="25"/>
-                <h3> 
-                    Bench Press
-                </h3>
-            </div>
-        <table>
-
-            <thead>
-            <tr>
-                <th>Set</th>
-                <th>Weight</th>
-                <th>Reps</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>1</td>
-                <td>
-                    <input type="text" placeholder="195"/>
-                </td>
-                <td>
-                    <input type="text" placeholder="8"/>
-                </td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>
-                    <input type="text" placeholder="--"/>
-                </td>
-                <td>
-                    <input type="text" placeholder="--"/>
-                </td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>
-                    <input type="text" placeholder="--"/>
-                </td>
-                <td>
-                    <input type="text" placeholder="--"/>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-        <button id="add-set-btn" type="button">+ Add Set</button>
-        <button id="add-exercise-btn" type="button">Add Exercise</button>
-        </div>
+    <ExerciseRecord />
 
 
     </form>
+            <button id="add-exercise-btn" type="button">Add Exercise</button>
     </main>
   );
 }
