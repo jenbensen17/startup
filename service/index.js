4dbc974f4916bdc29cbe052398af2afb0f9cc2cb
jenbensen17 <jenbensen17@gmail.com>
2025-03-04 13:39:34 -0700

    Setup index.js and vite config

---
+++ service/index.js
@@ -0,0 +1,5 @@
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
