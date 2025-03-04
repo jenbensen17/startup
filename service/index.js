24125c849674cb91f3e9be10fc638214061cdc80
jenbensen17 <jenbensen17@gmail.com>
2025-03-04 14:02:08 -0700

    Add createAuth endpoint

--- service/index.js
+++ service/index.js
@@ -1,6 +1,10 @@
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
+const express = require('express');
+const app = express();
+
+const authCookieName = 'token';
 
 // The users are saved in memory and disappear whenever the service is restarted.
 let users = [];
@@ -13,9 +17,49 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
 
-let apiRouter = express.Router();
+// Router for service endpoints
+var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
   
+//CreateAuth a new user
+apiRouter.post('/auth/create', async(req, res) => {
+    if(await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.email, req.body.password);
+        setAuthCookie(res, user.token);
+        res.send({email: user.email})
+    }
+})
+
+
+async function findUser(field, value) {
+    if (!value) return null;
+  
+    return users.find((u) => u[field] === value);
+  }
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+    return user;
+}
+
+
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
   });
