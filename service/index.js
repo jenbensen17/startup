4ceecaff9e6f208f0dd7aa33b9b224626d8e1f6b
jenbensen17 <jenbensen17@gmail.com>
2025-03-04 13:52:36 -0700

    Set up express

--- service/index.js
+++ service/index.js
@@ -1,5 +1,22 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+// The users are saved in memory and disappear whenever the service is restarted.
+let users = [];
+let workouts = [];
+
+
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+  
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
