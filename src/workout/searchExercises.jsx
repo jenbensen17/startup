1943d535991252b8589dec46d2f2996fed0f7775
jenbensen17 <jenbensen17@gmail.com>
2025-03-06 12:39:58 -0700

    Initial 3rd party api integration

--- src/workout/searchExercises.jsx
+++ src/workout/searchExercises.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import './search.css'
 
 export function SearchExercises(props) {
-    const exercises = ["Bench Press", "Squat", "Deadlift"];
+    const [exercises, setExercises] = React.useState([]);
     const [search, setSearch] = React.useState("");
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        const fetchExercises = async () => {
+            const response = await fetch("https://wger.de/api/v2/exercise/?language=2");
+            const data = await response.json();
+            console.log(data)
+            setExercises(data.results)
+        }
+        fetchExercises();
+    }, [])
 
     return (
         <div className="overlay">
@@ -16,12 +27,17 @@ export function SearchExercises(props) {
                 onChange={(e) => setSearch(e.target.value)}
                 ></input>
              <ul className="modal-list">
-                {exercises.filter((e) => e.toLowerCase().includes(search.toLowerCase()))
-                    .map((exercise, index) => (
-                    <li key={index} className="modal-item" onClick={() => props.onSelect(exercise)}>
-                        {exercise}
-                    </li>
-                    ))}
+                {(
+                        exercises
+                            .filter((exercise) =>
+                                exercise.name.toLowerCase().includes(search.toLowerCase())
+                            )
+                            .map((exercise) => (
+                                <li key={exercise.id} className="modal-item" onClick={() => props.onSelect(exercise.name)}>
+                                    {exercise.name}
+                                </li>
+                            ))
+                    )}
                 </ul>
             <button className="modal-close" onClick={props.onClose}>Close</button>
             </div>
