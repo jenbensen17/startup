9f8ea5e975679c514824ac7cd269b08f6324dff0
jenbensen17 <jenbensen17@gmail.com>
2025-03-25 14:47:47 -0600

    Add database requests

--- service/database.js
+++ service/database.js
@@ -18,3 +18,46 @@ const maxLiftsCollection = db.collection('max-lifts')
       process.exit(1);
     }
   })();
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+    await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+    await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addWorkout(workout) {
+    return workoutCollection.insertOne(workout);
+}
+
+async function getWorkouts(userEmail) {
+    return workoutCollection.find({userEmail}).toArray();
+}
+
+async function getMaxLifts(userEmail) {
+    return maxLiftsCollection.findOne({userEmail});
+}
+
+async function updateMaxLifts(userEmail, maxLifts) {
+    return maxLiftsCollection.updateOne({userEmail}, {$set: maxLifts}, {upsert: true});
+}
+
+module.exports = { 
+    getUser, 
+    getUserByToken, 
+    addUser, 
+    updateUser, 
+    addWorkout, 
+    getWorkouts, 
+    getMaxLifts, 
+    updateMaxLifts 
+};
