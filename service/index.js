27c46b5c99394de01cccce3e4d16958445c52c25
jenbensen17 <jenbensen17@gmail.com>
2025-03-25 15:13:11 -0600

    Add login and logout database connections

--- service/index.js
+++ service/index.js
@@ -47,6 +47,7 @@ apiRouter.post('/auth/login', async(req, res) => {
     if(user) {
         if(await bcrypt.compare(req.body.password, user.password)) {
             user.token = uuid.v4();
+            await DB.updateUser(user);
             setAuthCookie(res, user.token);
             res.send({email: user.email});
             return;
@@ -60,6 +61,7 @@ apiRouter.delete('/auth/logout', async (req, res) => {
     const user = await findUser('token', req.cookies[authCookieName]);
     if (user) {
       delete user.token;
+      DB.updateUser(user);
     }
     res.clearCookie(authCookieName);
     res.status(204).end();
